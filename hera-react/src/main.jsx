ebea9e228f2130b7cc0ea386f68092468f65b736
MOSTAFAEZZAT <mostafaezzat318@gmail.com>
2024-01-14 16:53:53 +0200

    Services

--- hera-react/src/main.jsx
+++ hera-react/src/main.jsx
@@ -4,14 +4,14 @@ import App from './pages/App.jsx'
 import Testimonials from './pages/Testimonials.jsx'
 import Stats from './pages/Stats.jsx'
 import TrustedBy from './pages/TrustedBy.jsx'
-import Features from './pages/Features.jsx'
+import Services from './pages/Features.jsx'
 import Footer from './pages/Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Features />
+    <Service />
     <TrustedBy />
     <Stats />
     <Testimonials />
