9933ad4e8bccaf3f13909430ee82cfe7c2d00d71
MOSTAFAEZZAT <mostafaezzat318@gmail.com>
2024-01-14 16:59:36 +0200

    Services

--- hera-react/src/main.jsx
+++ hera-react/src/main.jsx
@@ -4,7 +4,7 @@ import App from './pages/App.jsx'
 import Testimonials from './pages/Testimonials.jsx'
 import Stats from './pages/Stats.jsx'
 import TrustedBy from './pages/TrustedBy.jsx'
-import Services from './pages/Features.jsx'
+import Service from './pages/Features.jsx'
 import Footer from './pages/Footer.jsx'
 import './index.css'
 
--- hera-react/src/pages/Features.jsx
+++ hera-react/src/pages/Features.jsx
@@ -1,4 +1,5 @@
 import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import React from "react";
 
 const features = [
     {
@@ -39,43 +40,42 @@ const features = [
     },
 ]
 
-export default function Features() {
-    return (
-        <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Everything you need to grow your business
-                    </p>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Empower your business with cutting-edge solutions at Hera.
-                        From dynamic software development to strategic marketing campaigns, we specialize in crafting digital excellence.
-                        Elevate your brand, engage your audience, and achieve unprecedented success. Discover the future of innovation with Hera.
-                    </p>
-                </div>
-                <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-                    <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-                        {features.map((feature) => (
-                            <div key={feature.name} className="relative pl-16">
-                                <dt className="text-base font-semibold leading-7 text-gray-900">
-                                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                                    </div>
-                                    {feature.name}
-                                </dt>
-                                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
-                            </div>
-                        ))}
-                    </dl>
-                </div>
-            </div>
-        </div>
-    )
-}
-import React from "react";
+// export function Features() {
+//     return (
+//         <div className="bg-white py-24 sm:py-32">
+//             <div className="mx-auto max-w-7xl px-6 lg:px-8">
+//                 <div className="mx-auto max-w-2xl lg:text-center">
+//                     <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+//                     <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+//                         Everything you need to grow your business
+//                     </p>
+//                     <p className="mt-6 text-lg leading-8 text-gray-600">
+//                         Empower your business with cutting-edge solutions at Hera.
+//                         From dynamic software development to strategic marketing campaigns, we specialize in crafting digital excellence.
+//                         Elevate your brand, engage your audience, and achieve unprecedented success. Discover the future of innovation with Hera.
+//                     </p>
+//                 </div>
+//                 <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+//                     <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+//                         {features.map((feature) => (
+//                             <div key={feature.name} className="relative pl-16">
+//                                 <dt className="text-base font-semibold leading-7 text-gray-900">
+//                                     <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+//                                         <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+//                                     </div>
+//                                     {feature.name}
+//                                 </dt>
+//                                 <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+//                             </div>
+//                         ))}
+//                     </dl>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export const Service = () => {
+export default function Service() {
     return (
         <section className="pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px]">
             <div className="container mx-auto">
