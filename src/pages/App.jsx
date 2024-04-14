1a1c1d500d45ad3591d30e97bfa98f9ee8d86d9f
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-04-14 05:30:42 +0200

    Adding react scroll to the side bar

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -85,6 +85,9 @@ export default function HeroSection() {
                 <div className="py-6 space-y-2">
                   {navigation.map((item) => (
                     <a
+                      onClick={() => {
+                        scrollToSection(item.scrollNumber);
+                      }}
                       key={item.name}
                       href={item.href}
                       className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
